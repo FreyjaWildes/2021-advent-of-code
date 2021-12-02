87946f6d0ac11b1c0efbe4fc5d7b3649aedacdd0
Freyja Wildes <freyja_wildes@pm.me>
2021-12-02 21:29:56 +0100

    Day 1, second star

--- 01/sonnar.py
+++ 01/sonnar.py
@@ -1,12 +1,24 @@
 input = open("input.txt", 'r')
-increase_count = 0
-previous_depth = -1
 lines = input.readlines()
-for depthLine in lines:
-    depth = int(depthLine)
-    if (previous_depth >= 0 and (previous_depth - depth) < 0):
-        increase_count += 1
-    previous_depth = depth
 
+if len(lines) < 4 :
+    exit(0)
+
+previous_depth = -1
+increase_count = 0
+"""
+Let the lines be numbered: w1 w2 w3 w4
+and windows be named A, B, C...
+A-B = w1 - w4
+B-C = w2 - w5
+"""
+window = [-1,-1,-1,-1]
+for l in range(len(lines)):
+    depth = int(lines[l])
+    previous_depth = window[l % 3] 
+    if (previous_depth > -1 and previous_depth - depth < 0):
+        increase_count += 1
+    window[l % 3] = depth
+        
 print("Increased: {0}".format(increase_count))
 input.close()
